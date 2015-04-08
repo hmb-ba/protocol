62346fcc55f2397f0e6330c2fa9f081817f030d1
Marc Juchli <mail@marcjuch.li>
2015-04-08 22:40:16 +0200

    add missing files

---
+++ src/Kafka/Protocol.hs
@@ -0,0 +1,9 @@
+module Kafka.Protocol
+( module Kafka.Protocol.Types
+, module Kafka.Protocol.Parser
+, module Kafka.Protocol.Serializer
+) where
+
+import Kafka.Protocol.Types
+import Kafka.Protocol.Parser
+import Kafka.Protocol.Serializer
