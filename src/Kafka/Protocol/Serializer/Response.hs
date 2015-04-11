98c37e199cea29952b39a11ca1c1961237e83780
Marc Juchli <mail@marcjuch.li>
2015-04-12 01:31:43 +0200

    fetchresponse serializer done

--- src/Kafka/Protocol/Serializer/Response.hs
+++ src/Kafka/Protocol/Serializer/Response.hs
@@ -1,6 +1,6 @@
 module Kafka.Protocol.Serializer.Response
-( 
- buildPrResponseMessage
+( buildPrResponseMessage
+, buildFtRsMessage
 ) where 
 
 import qualified Data.ByteString as BS
@@ -9,6 +9,18 @@ import qualified Network.Socket.ByteString.Lazy as SBL
 import Network.Socket
 import Data.Binary.Put
 import Kafka.Protocol.Types
+import Kafka.Protocol.Serializer.Data
+
+
+buildList :: (a -> BL.ByteString) -> [a] -> BL.ByteString
+buildList builder [] = BL.empty 
+buildList builder (x:xs) = BL.append (builder x) (buildList builder xs)
+
+buildRsMessage :: (Response -> BL.ByteString) -> ResponseMessage -> BL.ByteString
+buildRsMessage rsBuilder rm = runPut $ do
+  putWord32be       $ rsCorrelationId rm
+  putWord32be       $ rsNumResponses rm
+  putLazyByteString $ buildList rsBuilder $ rsResponses rm
 
 --------------------
 -- Produce Response (Pr)
@@ -41,8 +53,26 @@ buildPrResponseMessage e = runPut $ do
   putWord32be $ rsNumResponses e 
   putLazyByteString $ buildProduceResponses $ rsResponses e 
 
---sendProduceResponse :: Socket -> ResponseMessage -> IO() 
---sendProduceResponse socket responsemessage = do 
---  let msg = buildProduceResponseMessage responsemessage
---  SBL.sendAll socket msg
+--------------------
+-- FetchResponse
+--------------------
+buildFtPayload :: RsFtPayload -> BL.ByteString
+buildFtPayload p = runPut $ do 
+  putWord32be       $ rsFtPartitionNumber p
+  putWord16be       $ rsFtErrorCode p
+  putWord64be       $ rsFtHwMarkOffset p
+  putWord32be       $ rsFtMessageSetSize p
+  putLazyByteString $ buildMessageSet $ rsFtMessageSet p
+
+
+buildFtRs :: Response -> BL.ByteString
+buildFtRs rs = runPut $ do
+  putWord16be       $ rsFtTopicNameLen rs
+  putLazyByteString $ BL.fromStrict(rsFtTopicName rs)
+  putWord32be       $ rsFtNumsPayloads rs
+  putLazyByteString $ buildList buildFtPayload $ rsFtPayloads rs
+
+
+buildFtRsMessage :: ResponseMessage -> BL.ByteString
+buildFtRsMessage rm = buildRsMessage buildFtRs rm
 
