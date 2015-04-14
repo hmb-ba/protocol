03447a28659e5cc4269f8082cae6454da3d8e5ca
Marc Juchli <mail@marcjuch.li>
2015-04-14 10:32:51 +0200

    begin with consumer

--- src/Kafka/Client/Consumer.hs
+++ src/Kafka/Client/Consumer.hs
@@ -1,3 +1,33 @@
 module Kafka.Client.Consumer 
 (
-) where 
+) where
+
+-- packTopic :: String -> [Partition] -> Topic
+-- packTopic t ps = Topic
+--   (fromIntegral $ BS.length t)
+--   t
+--   (fromIntegral $ length ps)
+--   ps
+-- 
+-- packFtRequest :: -> FetchRequest
+-- packFtRequest = FetchRequest 
+--   0
+--   0
+--   -- todo: weiter hier
+-- 
+-- packFtRequestMessage :: InputFt -> RequestMessage
+-- packFtRequestMessage iM = Requestmessage {
+--       rqSize = fromIntegral $ (BL.length $ buildProduceRequestMessage packFtRequest )
+--           + 2 -- reqApiKey
+--           + 2 -- reqApiVersion
+--           + 4 -- correlationId 
+--           + 2 -- clientIdLen
+--           + (fromIntegral $ BS.length $ inputClientId iM) --clientId
+--     , rqApiKey = 0
+--     , rqApiVersion = 0
+--     , rqCorrelationId = 0
+--     , rqClientIdLen = fromIntegral $ BS.length $ inputClientId iM
+--     , rqClientId = inputClientId iM
+--     , rqRequest = produceRequest
+--   }
+
