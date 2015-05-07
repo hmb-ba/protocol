b65e0bc45b86147028e783226d91ab73e8fe6131
Marc Juchli <mail@marcjuch.li>
2015-05-07 16:36:49 +0200

    allow list of bs as payload

--- src/Kafka/Client/Producer.hs
+++ src/Kafka/Client/Producer.hs
@@ -8,25 +8,24 @@ import Kafka.Protocol
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Char8 as BC
 import Data.Digest.CRC32
 import Data.Binary.Get
 
 import qualified Control.Exception as E 
 
-packPrRqMessage :: (String, String, Int, String) -> RequestMessage
+packPrRqMessage :: (BS.ByteString, BS.ByteString, Int, [BS.ByteString]) -> RequestMessage
 packPrRqMessage (client, topic, partition, inputData) = RequestMessage {
       rqSize = fromIntegral $ (BL.length $ buildProduceRequest produceRequest )
           + 2 -- reqApiKey
           + 2 -- reqApiVersion
           + 4 -- correlationId 
           + 2 -- clientIdLen
-          + (fromIntegral $ length client) --clientId
+          + (fromIntegral $ BS.length client) --clientId
     , rqApiKey = 0
     , rqApiVersion = 0
     , rqCorrelationId = 0
-    , rqClientIdLen = fromIntegral $ length client
-    , rqClientId = BC.pack client
+    , rqClientIdLen = fromIntegral $ BS.length client
+    , rqClientId = client
     , rqRequest = produceRequest
   }
   where produceRequest = ProduceRequest
@@ -35,18 +34,22 @@ packPrRqMessage (client, topic, partition, inputData) = RequestMessage {
                           (fromIntegral $ length [packTopic])
                           [packTopic]
         packTopic = RqTopic
-                          (fromIntegral $ length topic) 
-                          (BC.pack topic)
+                          (fromIntegral $ BS.length topic) 
+                          topic
                           (fromIntegral $ length [packPartition])
                           ([packPartition])
         packPartition = RqPrPartition
                           (fromIntegral partition)
-                          (fromIntegral $ BL.length $ buildMessageSet packMessageSet)
-                          [packMessageSet]
-        packMessageSet = MessageSet
+                          (fromIntegral $ BL.length $ buildMessageSets ms)
+                          ms
+        ms = (map packMessageSet inputData)
+
+packMessageSet :: BS.ByteString -> MessageSet
+packMessageSet bs = MessageSet
                           0
                           (fromIntegral $ BL.length $ buildMessage packMessage)
                           packMessage
+  where
         packMessage = Message
                           (crc32 $ buildPayload packPayload)
                           packPayload
@@ -54,8 +57,8 @@ packPrRqMessage (client, topic, partition, inputData) = RequestMessage {
                           0
                           0
                           0
-                          (fromIntegral $ length inputData)
-                          (BC.pack inputData)
+                          (fromIntegral $ BS.length bs)
+                          bs
 
 
 -------------------
