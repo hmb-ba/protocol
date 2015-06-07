46e6ed300e65b8f84ca9cc9f1d0bfc2df78a9fd8
SherwoodCH <lorenz.wolf@bluewin.ch>
2015-06-07 15:53:53 +0200

    add functions to reduce "fromInegral" in code

--- src/Kafka/Client.hs
+++ src/Kafka/Client.hs
@@ -32,21 +32,20 @@ module Kafka.Client
 , textToData
 ) where
 
-import Kafka.Protocol
+import qualified Control.Exception as E
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy.Char8 as C
-import Data.Digest.CRC32
 import Data.Binary.Get
 import Data.Binary.Put
-import Data.Tagged
-
-import qualified Control.Exception as E
-
+import Data.Digest.CRC32
 import qualified Data.Text as T
 import Data.Text.Encoding
+import Data.Word
+
+import Kafka.Protocol
 
 import Network.Socket
 import qualified Network.Socket.ByteString.Lazy as SBL
@@ -111,24 +110,36 @@ stringToData s = BC.pack s
 textToData :: T.Text -> BS.ByteString
 textToData t = encodeUtf8 t
 
+encodedLength :: Put -> Word32
+encodedLength p = fromIntegral $ BL.length $ runPut p
+
+byteLength :: BS.ByteString -> Word32
+byteLength b = fromIntegral $ BS.length b
+
+stringLength :: BS.ByteString -> Word16
+stringLength b = fromIntegral $ BS.length b
+
+arrayLength :: [a] -> Word32
+arrayLength xs = fromIntegral $ length xs
+
 --------------------
 --Pack Functions
 --------------------
 -- | Pack a protocol conform RequestMessage for Produce API
 packPrRqMessage :: Head -> [ToTopic] -> RequestMessage
 packPrRqMessage head ts = RequestMessage
-  { rqSize = fromIntegral $ (BL.length $ runPut $ buildProduceRequest produceRequest )
+  { rqSize = (encodedLength $ buildProduceRequest produceRequest)
           + 2 -- reqApiKey
           + 2 -- reqApiVersion
           + 4 -- correlationId
           + 2 -- clientIdLen
-          + (fromIntegral $ BS.length $ client head ) --clientId
-    , rqApiKey = 0
-    , rqApiVersion = fromIntegral $ apiV head
-    , rqCorrelationId = fromIntegral $ corr head
-    , rqClientIdLen = fromIntegral $ BS.length $ client head
-    , rqClientId = client head
-    , rqRequest = produceRequest
+          + (byteLength $ client head ) --clientId
+    , rqApiKey          = 0
+    , rqApiVersion      = fromIntegral $ apiV head
+    , rqCorrelationId   = fromIntegral $ corr head
+    , rqClientIdLen     = stringLength $ client head
+    , rqClientId        = client head
+    , rqRequest         = produceRequest
   }
   where
     produceRequest = ProduceRequest
@@ -138,19 +149,19 @@ packPrRqMessage head ts = RequestMessage
           (pts)
     pts = (map packTopic $ ts)
     packTopic (ToTopic t ps) = RqTopic
-          (fromIntegral $ BS.length $ t)
+          (stringLength $ t)
           t
           (fromIntegral $ length $ pps ps)
           (pps ps)
     pps ps = (map packPartition ps)
     packPartition (ToPart i ms) = RqPrPartition
           (fromIntegral $ i)
-          (fromIntegral $ BL.length $ runPut $ buildMessageSets $ pms ms)
+          (encodedLength $ buildMessageSets $ pms ms)
           (pms ms)
     pms ms = (map packMessageSet ms)
     packMessageSet bs = MessageSet
           0
-          (fromIntegral $ BL.length $ runPut $ buildMessage $ packMessage bs)
+          (encodedLength $ buildMessage $ packMessage bs)
           (packMessage bs)
     packMessage bs = Message
           (crc32 $ runPut $ buildPayload $ packPayload bs)
@@ -166,16 +177,16 @@ packPrRqMessage head ts = RequestMessage
 -- | Pack a protocol conform RequestMessage for Fetch API
 packFtRqMessage :: Head -> [FromTopic] -> RequestMessage
 packFtRqMessage head ts = RequestMessage
-  { rqSize = (fromIntegral $ (BL.length $ runPut $ buildFetchRequest $ packFtRequest)
+  { rqSize = (encodedLength $ buildFetchRequest $ packFtRequest)
           + 2 -- reqApiKey
           + 2 -- reqApiVersion
           + 4 -- correlationId
           + 2 -- clientIdLen
-          + (fromIntegral $ BS.length $ client head))
+          + (byteLength $ client head)
     , rqApiKey = 1
     , rqApiVersion = fromIntegral $ apiV head
     , rqCorrelationId = fromIntegral $ corr head
-    , rqClientIdLen = fromIntegral $ BS.length $ client head
+    , rqClientIdLen = stringLength $ client head
     , rqClientId = client head
     , rqRequest = packFtRequest
   }
@@ -196,21 +207,21 @@ packFtRqMessage head ts = RequestMessage
     packPartition (FromPart p o) = RqFtPartition
           (fromIntegral p)
           (fromIntegral o)
-          1048576 --TODO Maxbytes as input from client
+          1048576 -- Maxbytes
 
 -- | Pack a protocol conform RequestMessage for Metadata API
 packMdRqMessage :: Head -> [OfTopic] -> RequestMessage
 packMdRqMessage head ts = RequestMessage
-  { rqSize = (fromIntegral $ (BL.length $ runPut $ buildMetadataRequest $ packMdRequest)
+  { rqSize = (encodedLength $ buildMetadataRequest $ packMdRequest)
           + 2 -- reqApiKey
           + 2 -- reqApiVersion
           + 4 -- correlationId
           + 2 -- clientIdLen
-          + (fromIntegral $ BS.length $ client head))
+          + (byteLength $ client head)
     , rqApiKey = 3
     , rqApiVersion = fromIntegral $ apiV head
     , rqCorrelationId = fromIntegral $ corr head
-    , rqClientIdLen = fromIntegral $ BS.length $ client head
+    , rqClientIdLen = stringLength $ client head
     , rqClientId = client head
     , rqRequest = packMdRequest
   }
